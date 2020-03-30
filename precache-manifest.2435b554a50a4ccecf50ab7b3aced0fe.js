e5aa69f8c07e2412ae2b376dbdb074f917d8e985
abdurrahman <rajab-2@windowslive.com>
2020-03-30 12:44:57 +0300

    Updates

---
+++ precache-manifest.2435b554a50a4ccecf50ab7b3aced0fe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d2e5e330cec02d47aea0cc52ed802a8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0b4cb24d4ca3e2d9a140",
+    "url": "/portfolio/static/css/main.e74facf6.chunk.css"
+  },
+  {
+    "revision": "e81a673daab1c9075c5b",
+    "url": "/portfolio/static/js/2.86fb4950.chunk.js"
+  },
+  {
+    "revision": "d93e154037098bd00aeaec5110580573",
+    "url": "/portfolio/static/js/2.86fb4950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b4cb24d4ca3e2d9a140",
+    "url": "/portfolio/static/js/main.684f17e7.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "39a86c4c6bf61b94f0e40ec7751353f5",
+    "url": "/portfolio/static/media/Turing.39a86c4c.PNG"
+  },
+  {
+    "revision": "48ec77c8a73b70110c897a3db3597b91",
+    "url": "/portfolio/static/media/Visa.48ec77c8.PNG"
+  },
+  {
+    "revision": "a5fcf313df8423b0c3bd4bf22ed3d05c",
+    "url": "/portfolio/static/media/chatBot.a5fcf313.PNG"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/portfolio/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "40e388af776bf172d6ffccd048ad27bd",
+    "url": "/portfolio/static/media/searchEngine.40e388af.PNG"
+  }
+]);
